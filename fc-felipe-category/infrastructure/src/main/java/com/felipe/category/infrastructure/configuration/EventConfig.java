95b188cde305fff492482e49a3d2760b10dd9931
FelipeTedardi <felipe.tedardi@kroton.com.br>
2025-03-14 22:37:32 -0300

    adicao da parte de security

--- fc-felipe-category/infrastructure/src/main/java/com/felipe/category/infrastructure/configuration/EventConfig.java
+++ fc-felipe-category/infrastructure/src/main/java/com/felipe/category/infrastructure/configuration/EventConfig.java
@@ -4,15 +4,26 @@ import com.felipe.category.infrastructure.configuration.annotations.VideoCreated
 import com.felipe.category.infrastructure.configuration.properties.amqp.QueueProperties;
 import com.felipe.category.infrastructure.services.EventService;
 import com.felipe.category.infrastructure.services.impl.RabbitEventService;
+import com.felipe.category.infrastructure.services.local.InMemoryEventService;
 import org.springframework.amqp.rabbit.core.RabbitOperations;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class EventConfig {
 
     @Bean
     @VideoCreatedQueue
+    @Profile({"development"})
+    EventService localVideoCreatedEventService() {
+        return new InMemoryEventService();
+    }
+
+    @Bean
+    @VideoCreatedQueue
+    @ConditionalOnMissingBean
     EventService videoCreatedEventService(
             @VideoCreatedQueue final QueueProperties props,
             final RabbitOperations ops
---
+++ fc-felipe-category/infrastructure/src/main/java/com/felipe/category/infrastructure/services/local/InMemoryEventService.java
@@ -0,0 +1,16 @@
+package com.felipe.category.infrastructure.services.local;
+
+import com.felipe.category.infrastructure.configuration.json.Json;
+import com.felipe.category.infrastructure.services.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InMemoryEventService implements EventService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryEventService.class);
+
+    @Override
+    public void send(Object event) {
+        LOG.info("Event was observed: {}", Json.writeValueAsString(event));
+    }
+}
